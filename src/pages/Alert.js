3dc9975cdf0a48b456c387137620de7d6766db99
io <ioana.pascu@history.bucharest.university>
2026-08-05 19:45:30 +0300

    added aria to alert

--- src/pages/Alert.js
+++ src/pages/Alert.js
@@ -20,10 +20,10 @@ const Alert = ({type, year, closeAlert}) => {
 
 
   return (
-  <div className={`alertWrapper ${isFading ? 'fade-out' : ''}`}>
-    <button onClick={closeAlert}>×</button>
+  <div role="status" aria-atomic="true" aria-live="polite" className={`alertWrapper ${isFading ? 'fade-out' : ''}`}>
+    <button onClick={closeAlert} aria-label="Close notification">×</button>
       <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcXd3OXEydGI4bGJieWtkang3MGZmeDEya2FubXBsMHJyMndiZzlpaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/lIC8sATLdur9pvyR8k/giphy.gif"
-        alt="flying white bat gif" width="70px" height="70px"></img>
+        alt="flying white bat gif" width="70px" height="70px" aria-hidden="true"></img>
 
         <p>{type}, {year} successfully added to cart!</p>
   </div>
